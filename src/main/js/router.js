72c355855b093fadf1bfcc445857ac6ef400807a
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-07-07 18:54:48 +0300

    styles

--- src/main/js/router.js
+++ src/main/js/router.js
@@ -1,11 +1,11 @@
 async function navigate(moduleName) {
   const res = await fetch(`./src/main/html/module/${moduleName}.html`);
-  document.getElementById('pageContent').innerHTML = await res.text();
+  document.getElementById('moduleView').innerHTML = await res.text();
 }
 
 function initRouter() {
-  document.getElementById('leftBarNav').addEventListener('click', e => {
-    const item = e.target.closest('.navItem[data-page]');
+  document.getElementById('leftBarItemsView').addEventListener('click', e => {
+    const item = e.target.closest('.leftBarItemStyle[data-page]');
     if (item?.dataset.page) navigate(item.dataset.page);
   });
 }
