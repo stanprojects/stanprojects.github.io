5639af91e2854fae61adb919f1b9330ddde81239
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-07-13 19:28:46 +0300

    modules

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -4,19 +4,44 @@ const Colors = Object.freeze({
   Light: 'light',
 });
 
+const ModuleFoo = Object.freeze({
+  id: 'foo',
+  button: 'foo button',
+  title: 'foo title',
+  path: './src/main/html/foo.html',
+});
+
+const ModuleBar = Object.freeze({
+  id: 'bar',
+  button: 'bar button',
+  title: 'bar title',
+  path: './src/main/html/bar.html',
+});
+
 let _colors = undefined
 let _opened = undefined
+let _module = undefined
 
 const ColorsSwitch = document.getElementById('ColorsSwitch')
 const MenuSwitch = document.getElementById('MenuSwitch')
 const Scrim = document.getElementById('Scrim')
+const Modules = document.getElementById('Modules')
 
 function colorsOf(name) {
     return Object.values(Colors).includes(name) ? name : Colors.Dark;
 }
 
-function getState({ colors = _colors } = {}) {
-    return `#colors=${colors}`
+function moduleOf(id) {
+    switch (id) {
+        case ModuleBar.id:
+            return ModuleBar;
+        default:
+            return ModuleFoo;
+    }
+}
+
+function getState({ module = _module, colors = _colors } = {}) {
+    return `#module=${_module.id}&colors=${colors}`
 }
 
 function renderColors(colors) {
@@ -32,7 +57,22 @@ function renderOpened(opened) {
     Scrim.classList.toggle('opened', _opened === true)
 }
 
-function onStateChange({ colors = _colors, opened = _opened }, needsToPush = false) {
+function renderModules(selected) {
+    Modules.querySelectorAll('.ModuleItem').forEach((it) => {
+        it.classList.toggle('selected', it.dataset.id === selected.id)
+    })
+}
+
+function renderModule(module) {
+    _module = module
+    renderModules(module)
+//    renderModuleScreen(selected)
+}
+
+function onStateChange({ module = _module, colors = _colors, opened = _opened }, needsToPush = false) {
+    if (_module !== module) {
+        renderModule(module)
+    }
     if (_colors !== colors) {
         renderColors(colors)
     }
@@ -52,7 +92,8 @@ function onStateChange({ colors = _colors, opened = _opened }, needsToPush = fal
 function onPopState(opened) {
     const params = new URLSearchParams(location.hash.slice(1))
     const colors = colorsOf(params.get('colors'))
-    onStateChange({ colors: colors, opened: opened })
+    const module = moduleOf(params.get('module'))
+    onStateChange({ colors: colors, module: module, opened: opened })
 }
 
 ColorsSwitch.addEventListener('click', () => {
@@ -68,6 +109,14 @@ MenuSwitch.addEventListener('click', () => {
 Scrim.addEventListener('click', () => {
     if (_opened === true) {
         onStateChange({ opened: false })
+    }
+})
+
+Modules.addEventListener('click', (event) => {
+    const it = event.target.closest('.ModuleItem')
+    if (!it) return
+    if (_module.id !== it.dataset.id) {
+        onStateChange({ module: moduleOf(it.dataset.id), opened: false }, true)
     }
 })
 
